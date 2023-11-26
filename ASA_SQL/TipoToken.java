408e87ad4f54b527fa47373f00649e7dea3d97b0
Julio Juseppe <105829717+juliooooo22@users.noreply.github.com>
2023-11-26 17:30:07 -0600

    Analizador

---
+++ ASA_SQL/TipoToken.java
@@ -0,0 +1,15 @@
+
+public enum TipoToken {
+    IDENTIFIER,
+
+    // Palabras reservadas
+    SELECT, FROM, DISTINCT,
+
+    // Caracteres
+    COMMA, DOT, STAR,
+
+    // ERROR
+    ERROR,
+    // Final de cadena
+    EOF
+} 
